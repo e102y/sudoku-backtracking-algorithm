8c4bb3586385cdfbd7ae1057254b298a6b797e4f
devin t brown <toel3my@gmail.com>
2018-08-28 13:47:44 -0700

    board class added with get row col and show method

--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,28 @@
 
+class sudokuBoard():
+    def __init__(self, filename):
+        with open( filename, 'r' ) as f:
+            self.plines = [line.split() for line in f]
+    
+    def show(self):
+        for line in self.plines:
+            print(line)
 
+    def getRow(self,r):
+        return self.plines[r]
 
+    def getCol(self,c):
+        tmp = []
+        for row in range(9):
+            tmp.append(self.plines[row][c])
+        return tmp
+            
 
 def main():
-    filename = input('Enter sudoku puzzle file name: ')
-    with open( filename, 'r' ) as f:
-       plines = [line.split() for line in f]
-    
+    #TODO input('Enter sudoku puzzle filename: ') 
+    filename = 'puzzle'
+    b = sudokuBoard(filename)
+    print( b.getCol(1) )
 
 if __name__ == "__main__":
     main()
